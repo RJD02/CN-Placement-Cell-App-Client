7583af546a9d24b70e4578f345492967996adc76
RJD02 <dulangeraviraj@gmail.com>
2023-08-20 11:26:16 +0530

    Replace html table with component Table

--- src/pages/Interview.tsx
+++ src/pages/Interview.tsx
@@ -1,9 +1,65 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { IInterview } from "./Interviews";
+import makeRequest from "../utils/makeRequest";
+import Layout from "./Layout";
+import { IStudent } from "./Students";
+import Table from "../components/Table";
+
+interface IResultForStudentsWithInterviewId {
+  _id: string;
+  interview: string;
+  result: string;
+  student: IStudent;
+}
 
 const Interview = () => {
+  const [interview, setInterview] = useState<IInterview>();
+  const [students, setStudents] = useState<IStudent[]>([]);
   const { id } = useParams();
-  console.log(id);
-  return <div>Hello World</div>;
+  useEffect(() => {
+    const fetchInterview = async () => {
+      const data = await makeRequest("/interview/" + id);
+      const dateOfInterview = new Date(data.data.dateOfInterview);
+      const date = `${dateOfInterview.getDate()}/${
+        dateOfInterview.getMonth() + 1
+      }/${dateOfInterview.getFullYear()}`;
+      setInterview({ ...data.data, dateOfInterview: date });
+    };
+
+    const fetchStudents = async () => {
+      const data = await makeRequest("/result/" + id + "/students");
+      setStudents(
+        data.data.map((b: IResultForStudentsWithInterviewId) => b.student)
+      );
+    };
+
+    fetchInterview();
+    fetchStudents();
+  }, [id]);
+  console.log(students);
+  console.log(interview);
+  return (
+    <Layout>
+      <div className="interview-details">
+        <h1 className="text-2xl mt-8">{interview?.companyName}</h1>
+        <h4 className="text-blue-600 text-xl mt-1">{interview?.position}</h4>
+        <h5 className="text-gray-700 mt-1">
+          {interview?.dateOfInterview.toString()}
+        </h5>
+      </div>
+      <div className="students mt-4">
+        <h3 className="text-xl">Students assigned to interview</h3>
+        {students.length > 0 && (
+          <Table
+            headers={["name", "college", "status", "batch"]}
+            data={students}
+            route="students"
+          />
+        )}
+      </div>
+    </Layout>
+  );
 };
 
 export default Interview;
--- src/pages/Interviews.tsx
+++ src/pages/Interviews.tsx
@@ -2,30 +2,52 @@ import Layout from "./Layout";
 import { useState, useEffect } from "react";
 import makeRequest from "../utils/makeRequest";
 import { Link } from "react-router-dom";
+import CreateInterview from "../components/CreateInterview";
+import Table from "../components/Table";
 
-interface IInterview {
+export interface IInterview {
   companyName: string;
   position: string;
-  dateOfInterview: Date;
-  _id: string;
+  dateOfInterview: Date | string;
+  _id?: string;
 }
 
 const Interviews = () => {
   const [interviews, setInterviews] = useState<IInterview[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [isAddInterviewClicked, setIsAddInterviewClicked] = useState(false);
+
+  const addInterviewHandler = () => {
+    console.log("Clicked");
+    setIsAddInterviewClicked(true);
+  };
+
   useEffect(() => {
     const getInterviews = async () => {
       const data = await makeRequest("/interview");
-      setInterviews(data.data);
+      const studentData = data.data.map((d: IInterview) => {
+        const date = new Date(d.dateOfInterview);
+        return {
+          ...d,
+          dateOfInterview: `${date.getDate()}/${
+            date.getMonth() + 1
+          }/${date.getFullYear()}`,
+        };
+      });
+      setInterviews(studentData);
       setIsLoading(false);
     };
     getInterviews();
-  }, []);
+  }, [isAddInterviewClicked]);
+
   return (
     <Layout>
-      <div className="flex justify-between mt-4 items-center">
+      <div className="flex justify-between mt-4 items-center relative">
         <h1 className="text-2xl">All Interviews</h1>
-        <div className="add-interview cursor-pointer py-1 flex items-center rounded bg-blue-300 px-2">
+        <div
+          onClick={addInterviewHandler}
+          className="add-interview cursor-pointer py-1 flex items-center rounded bg-blue-300 px-2"
+        >
           <i className="fa-solid fa-lg fa-plus mr-1"></i>
           <p className="text-lg">Add interview</p>
         </div>
@@ -33,30 +55,17 @@ const Interviews = () => {
       {isLoading ? (
         <p>Loading...</p>
       ) : (
-        <table className="table-auto w-full text-center mt-4">
-          <thead>
-            <tr>
-              <th>Company</th>
-              <th>Position</th>
-              <th>Date of Interview</th>
-              <th>Update</th>
-            </tr>
-          </thead>
-          <tbody>
-            {interviews.map((interview) => (
-              <tr>
-                <td>{interview.companyName}</td>
-                <td>{interview.position}</td>
-                <td>{`${new Date(interview.dateOfInterview).getDate()}/${
-                  new Date(interview.dateOfInterview).getMonth() + 1
-                }/${new Date(interview.dateOfInterview).getFullYear()}`}</td>
-                <td>
-                  <Link className="underline text-blue-400" to={`/interviews/${interview._id}`}>Update</Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <Table
+          headers={["companyName", "position", "dateOfInterview"]}
+          data={interviews}
+          route="interviews"
+        />
+      )}
+      {isAddInterviewClicked && (
+        <CreateInterview
+          state={isAddInterviewClicked}
+          setState={setIsAddInterviewClicked}
+        />
       )}
     </Layout>
   );
